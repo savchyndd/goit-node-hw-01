bbb155f8b522ad6e4b43017a14d9c14ea8d2fb3e
Dmytro <savchyndd@gmail.com>
2023-06-30 16:42:01 +0200

    add step 4

--- contacts.js
+++ contacts.js
@@ -1,8 +1,8 @@
 const fs = require('fs/promises');
-const { nanoid } = require('nanoid');
 const path = require('path');
+const { nanoid } = require('nanoid');
 
-const contactsPath = path.join(__dirname, 'contacts.json');
+const contactsPath = path.join(__dirname, 'db/contacts.json');
 
 async function listContacts() {
   const data = await fs.readFile(contactsPath);
@@ -18,9 +18,11 @@ async function getContactById(contactId) {
 async function removeContact(contactId) {
   const contacts = await listContacts();
   const indexContact = contacts.findIndex(item => item.id === contactId);
-  if (index === -1) return null;
-  const [delContact] = contacts.splice(index, 1);
-  await fs.writeFile(contactsPath, json.stringify(contacts, null, 2));
+  if (indexContact === -1) {
+    return null;
+  }
+  const [delContact] = contacts.splice(indexContact, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
   return delContact;
 }
 
